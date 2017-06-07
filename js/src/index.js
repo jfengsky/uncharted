afe15183fdc0f8b2fcd7cb89e8f56003545524be
姜峰 <jiang.f@Ctrip.com>
2017-06-07 15:51:30 +0800

    auto-pkg

--- js/src/index.js
+++ js/src/index.js
@@ -1,2 +1,13 @@
 "use strict";
+/**
+ * https://ant.design/components/layout-cn/
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var antd_1 = require("antd");
+var Home = function () { return (React.createElement("div", null, "home")); };
+ReactDOM.render(React.createElement("div", null,
+    React.createElement(antd_1.Button, { type: "primary" }, "Primary"),
+    React.createElement(Home, null),
+    "1111"), document.getElementById('root'));
--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -1,2 +1,20 @@
+/**
+ * https://ant.design/components/layout-cn/
+ */
+
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Button } from 'antd'
+
+const Home = () => (
+  <div>home</div>
+)
+
+ReactDOM.render(
+  <div>
+    <Button type="primary">Primary</Button>
+    <Home />
+    1111
+  </div>,
+  document.getElementById('root')
+)
