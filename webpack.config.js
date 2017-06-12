8c353bc2134a1e76d84ead2451a12e0889bd23cf
姜峰 <jiang.f@Ctrip.com>
2017-06-12 11:14:13 +0800

    auto-pkg

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack');
+module.exports = {
+  entry: './ts/APP/index.tsx',
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist"
+  },
+  // Turn on sourcemaps
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  // Add minification
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+}
