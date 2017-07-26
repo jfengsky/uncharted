659ba21f42a5776f672de695f58bd16b772e7bfa
姜峰 <jiang.f@Ctrip.com>
2017-07-26 15:22:07 +0800

    save

--- ts/src/component/AddPage.tsx
+++ ts/src/component/AddPage.tsx
@@ -1,12 +1,54 @@
 import * as React from 'react'
+import { connect } from 'react-redux'
 
-interface ITProps {}
+interface ITProps {
+  pageTypeList: any[]
+}
 interface ITState {}
 
-export default class AddPage extends React.Component<ITProps, ITState> {
-  public render(){
+class AddPage extends React.Component<ITProps, ITState> {
+  constructor(props: ITProps) {
+    super(props)
+  }
+  public render():JSX.Element{
     return (
-      <div style={{clear: 'both'}}>AddPage</div>
+      <div style={{clear: 'both'}}>
+        <div>
+          <label>
+            页面标题：
+            <input type="text" />
+          </label>
+        </div>
+        <div>
+        <label>
+          页面类型：
+          <select></select>
+        </label>
+        </div>
+        <div>
+        <label>
+          页面描述：
+          <input type="text" />
+        </label>
+        </div>
+        <div>
+        <label>
+          页面源代码代码：
+          <textarea></textarea>
+        </label>
+        </div>
+        <button>保存</button>
+      </div>
     )
   }
 }
+
+const mapStateToProps = (state: any): any => ({
+  pageTypeList: state.pageTypeList
+})
+
+const mapDispatchToProps = (dispatch: any) =>({
+  
+})
+
+export default connect(mapStateToProps,mapDispatchToProps)(AddPage)
