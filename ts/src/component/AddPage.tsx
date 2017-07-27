0502008d87775f9df8d1eeaa5db11bff0a5b899a
姜峰 <jiang.f@Ctrip.com>
2017-07-27 09:38:28 +0800

    save

--- ts/src/component/AddPage.tsx
+++ ts/src/component/AddPage.tsx
@@ -4,37 +4,64 @@ import { connect } from 'react-redux'
 interface ITProps {
   pageTypeList: any[]
 }
-interface ITState {}
+interface ITState {
+  name: string
+  desc: string
+  pageTypeId: string
+  code: string
+}
 
 class AddPage extends React.Component<ITProps, ITState> {
   constructor(props: ITProps) {
     super(props)
+    this.state = {
+      name: '',
+      desc: '',
+      pageTypeId: '',
+      code: ''
+    }
   }
+  componentDidMount () {
+    console.log(location.href)
+  }
+  
   public render():JSX.Element{
+    let {
+      name,
+      desc,
+      pageTypeId,
+      code
+    } = this.state
     return (
       <div style={{clear: 'both'}}>
         <div>
           <label>
             页面标题：
-            <input type="text" />
+            <input value={name} onChange={ev => { this.setState({ name: ev.target.value }) }} type="text" />
           </label>
         </div>
         <div>
         <label>
           页面类型：
-          <select></select>
+          <select value={pageTypeId} onChange={ev => { this.setState({ pageTypeId: ev.target.value }) }}>
+            {
+              !!this.props.pageTypeList && this.props.pageTypeList.map((item, index) => (
+                <option key={index} value={item._id}>{item.name}</option>
+              ))
+            }
+          </select>
         </label>
         </div>
         <div>
         <label>
           页面描述：
-          <input type="text" />
+          <input value={desc} onChange={ev => { this.setState({ desc: ev.target.value }) }} type="text" />
         </label>
         </div>
         <div>
         <label>
           页面源代码代码：
-          <textarea></textarea>
+          <textarea value={code} onChange={ev => { this.setState({ code: ev.target.value }) }} />
         </label>
         </div>
         <button>保存</button>
